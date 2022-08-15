e8782d2364295ccd044b019da9ad187602ac5c4b
Can Engin <kcanengin@gmail.com>
2022-08-16 00:03:01 +0300

    Add project files.

---
+++ src/demoProjects/rentACar/Application/Services/Repositories/IBrandRepository.cs
@@ -0,0 +1,16 @@
+﻿using Core.Persistence.Repositories;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Repositories
+{
+    public interface IBrandRepository : IAsyncRepository<Brand>, IRepository<Brand>
+    {
+      
+    }
+}
